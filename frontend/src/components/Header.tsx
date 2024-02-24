6db258de3b58e83945fe194ffc126c7cd58cfaf8
nadia-minster <petrovanadia96@gmail.com>
2024-02-24 12:02:45 +1100

    first commit

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom"
+import { useAppContext } from "../context/AppContext";
+import SignOutButton from "./SignOutButton";
+
+const Header = () => {
+    const { isLoggedIn } = useAppContext()
+    return (
+        <div className="bg-green-900 py-6">
+            <div className="container mx-auto flex justify-between ">
+                <span className="text-3xl text-white font-bold tracking-tight  ">
+                    <Link to="/">Book Store</Link>
+                </span>
+                <span className="flex space-x-2">
+                    {isLoggedIn ? <>
+                        <Link className="flex items-center text-white px-3 font-bold hover:bg-green-600" to="/my-account">My Account</Link>
+                        <Link className="flex items-center text-white px-3 font-bold hover:bg-green-600" to="/my-books">Saved Books</Link>
+                        <Link className="flex items-center text-white px-3 font-bold hover:bg-green-600" to="/my-books">My Orders</Link>
+                        <SignOutButton />
+                    </> : <Link className="flex items-center text-white px-5 font-bold rounded-md hover:bg-green-600" to="/sign-in">Sign In</Link>}
+
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+    return (
+        <div className="bg-green-900 pb-16">
+            <div className="container mx-auto flex flex-col gap-2 ">
+                <h1 className="text-5xl text-white font-bold pt-5">Find your perfect book</h1>
+                <p className="text-xl text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
